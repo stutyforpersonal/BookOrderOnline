845f221bfd67a10f89b8624288488f1e2f4d9f04
willlizzy <68834611+willlizzy@users.noreply.github.com>
2020-07-27 15:05:34 +0800

    Add files via upload

---
+++ src/DomTest.java
@@ -0,0 +1,13 @@
+package com.wj;
+
+import java.lang.reflect.Field;
+
+public class DomTest {
+	public static void main(String[] args)  {
+		Field[] fields=User.class.getFields();
+		for(Field field:fields){
+			String s=field.getName();
+			System.out.println(s);
+		}
+	}
+}
---
+++ src/UserUtils.java
@@ -0,0 +1,15 @@
+package com.wj;
+
+public class UserUtils {
+	
+	public UserUtils() {
+		System.out.println("++++++++");
+	}
+	public String getUsercode(){
+		//User u = new User();
+		//u.setName("zhanag");
+		//u.setPassword("123456");
+		return "success";
+	}
+
+}
