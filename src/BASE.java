88fb9da0457f96609c35246fbf223036316d1673
jwang55 <jwang55@ebay.com>
2021-03-05 18:03:37 +0800

    Modified

--- src/BASE.java
+++ src/BASE.java
@@ -1,19 +1,5 @@
 package com.wj;
 
-public class User {
-	public String name;
-	public String password;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+public class BASE {
+	public static void main(String[] arggs){}
 }
